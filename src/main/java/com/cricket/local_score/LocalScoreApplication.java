be22709af7325fa2f1560c13f32351194f05e04b
Hareesh123NH <nagasarihareesh2005@gmail.com>
2025-02-07 14:29:26 +0530

    initial commit

---
+++ src/main/java/com/cricket/local_score/LocalScoreApplication.java
@@ -0,0 +1,13 @@
+package com.cricket.local_score;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalScoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cricket/local_score/LocalScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricket.local_score;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
